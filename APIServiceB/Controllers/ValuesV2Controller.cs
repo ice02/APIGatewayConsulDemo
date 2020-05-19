d35f41f2021dd1893f6da141247ef6e36f04b2aa
ice02 <iceman02@live.fr>
2020-05-19 18:23:00 +0200

    With load balancer, QOS and service with 2 versions

---
+++ APIServiceB/Controllers/ValuesV2Controller.cs
@@ -0,0 +1,19 @@
+﻿namespace APIServiceB.Controllers
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/v2/values")]
+    [ApiController]
+    public class ValuesV2Controller : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var port = Request.Host.Port;
+
+            return new string[] { "value1", "value2", port.Value.ToString(), "Netcore 2.2" };
+        }
+    }
+}
